25485e817227afa1f91219050d03a7f247d75f53
Md Nahid Chowdhury <mdnahidchy2017@gmail.com>
2026-01-18 00:21:42 +0600

    CU-86ew5q7pw - Configure Entity Framework Core & Initial Migration

---
+++ backend/src/ValenceHub.Persistence/Contexts/ValenceHubDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ValenceHub.Persistence.Contexts;
+
+public class ValenceHubDbContext : DbContext
+{
+    public ValenceHubDbContext(DbContextOptions<ValenceHubDbContext> options)
+        : base(options) { }
+}

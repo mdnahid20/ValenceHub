a5bcdd8ccae50fadbfb43b8cf6df7e37a9011190
Md Nahid Chowdhury <mdnahidchy2017@gmail.com>
2026-02-01 21:53:40 +0600

    CU-86ew8hd3u - Update time provider for domain layer

---
+++ backend/src/ValenceHub.Domain/Interfaces/IDateTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValenceHub.Domain.Interfaces
+{
+    public interface IDateTimeProvider
+    {
+        DateTime UtcNow { get; }
+    }
+}
---
+++ backend/src/ValenceHub.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ValenceHub.Domain.Interfaces;
+
+namespace ValenceHub.Infrastructure.Services
+{
+    public class SystemDateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
